4f60a91424005e4a76c5d9ce401fb63cd8859724
pavanchanduri <pavanch12321@gmail.com>
2025-05-25 17:05:59 +0530

    Added bundleId capability

--- src/test/java/AppiumTests/TestiOS.java
+++ src/test/java/AppiumTests/TestiOS.java
@@ -21,9 +21,10 @@ public class TestiOS {
         capabilities.setCapability("appium:automationName", AutomationName.IOS_XCUI_TEST);
         capabilities.setCapability("appium:deviceName", "iPhone 16 Pro Max");
         capabilities.setCapability("appium:udid", "E5F959CA-E2CB-4AC4-883B-1AF38139F824");
-        String appUrl = System.getProperty("user.dir")+ File.separator+"src"+File.separator+"main"+File.separator
-                +"resources"+File.separator+"UIKitCatalog-iphonesimulator.app";
-        capabilities.setCapability("appium:app", appUrl);
+//        String appUrl = System.getProperty("user.dir")+ File.separator+"src"+File.separator+"main"+File.separator
+//                +"resources"+File.separator+"UIKitCatalog-iphonesimulator.app";
+//        capabilities.setCapability("appium:app", appUrl);
+        capabilities.setCapability("appium:bundleId", "com.example.apple-samplecode.UICatalog");
 
         URL url = new URL("http://0.0.0.0:4723/");
         AppiumDriver driver = new IOSDriver(url, capabilities);
